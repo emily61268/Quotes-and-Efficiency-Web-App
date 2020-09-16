085dbccdd1e2921a0f050752f5641bf7f4d3c0be
Emily Huang <pengyuanhuang1@gmail.com>
2020-09-16 02:29:11 -0500

    Update from POST to PORT in app.listen.

--- app.js
+++ app.js
@@ -109,6 +109,6 @@ app.post("/", function(req, res) {
 
 
 
-app.listen(process.env.POST || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Running on port 3000.");
 });
