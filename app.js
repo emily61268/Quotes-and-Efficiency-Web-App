289f4b6af4b4aed48ed7c0131af936561ac57fa4
Emily Huang <pengyuanhuang1@gmail.com>
2020-09-16 02:31:18 -0500

    Remove fetch.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const http = require('http');
-const fetch = require("node-fetch");
 
 
 
